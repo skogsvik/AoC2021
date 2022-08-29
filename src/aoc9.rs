b7291bb952fab5a4f2e074280f2b63c4b72df677
Erik Silfverswärd <erik@silfversward.se>
2022-08-29 10:13:24 -0400

    Day 9

---
+++ src/aoc9.rs
@@ -0,0 +1,105 @@
+pub use crate::loaders::file_to_array2 as load;
+use ndarray::Array2;
+pub const DATA: &str = "input/aoc9";
+
+type Map = Array2<u32>;
+
+fn iter_low_points<'a>(floor: &'a Map) -> impl Iterator<Item = ((usize, usize), &u32)> + 'a {
+    let (n_row, n_col) = floor.dim();
+    floor.indexed_iter().filter(move |&((i_row, i_col), &pos)| {
+        iter_neighbours(i_row, i_col, n_row - 1, n_col - 1).all(|idx| floor[idx] > pos)
+    })
+}
+
+fn iter_neighbours(
+    row: usize,
+    col: usize,
+    max_row: usize,
+    max_col: usize,
+) -> impl Iterator<Item = (usize, usize)> {
+    let mut v = Vec::with_capacity(4);
+    if row > 0 {
+        v.push((row - 1, col));
+    }
+    if row < max_row {
+        v.push((row + 1, col));
+    }
+    if col > 0 {
+        v.push((row, col - 1));
+    }
+    if col < max_col {
+        v.push((row, col + 1));
+    }
+    v.into_iter()
+}
+
+fn find_unclaimed(claim_mask: &Array2<bool>) -> Option<(usize, usize)> {
+    claim_mask
+        .indexed_iter()
+        .find_map(|(idx, &val)| if !val { Some(idx) } else { None })
+}
+
+// TODO: use sobel gradient descent instead
+pub fn answer1(input: &Map) -> u32 {
+    iter_low_points(input).map(|(_, i)| i + 1).sum()
+}
+
+pub fn answer2(floor: &Map) -> usize {
+    // Map to keep track of cells to not check, starts as all the edges
+    let mut claimed = floor.map(|&height| height == 9);
+    let (n_row, n_col) = floor.dim();
+    let (max_row, max_col) = (n_row - 1, n_col - 1);
+    let mut basins = Vec::new();
+
+    while let Some(idx) = find_unclaimed(&claimed) {
+        claimed[idx] = true;
+        let mut basin = 1;
+        let mut new_cells = vec![idx];
+        while let Some((row, col)) = new_cells.pop() {
+            new_cells.extend(iter_neighbours(row, col, max_row, max_col).filter(|&idx| {
+                if claimed[idx] {
+                    return false; // Only care for unclaimed cells
+                }
+                claimed[idx] = true;
+                basin += 1;
+                true
+            }))
+        }
+        basins.push(basin);
+    }
+    basins.sort();
+    basins[basins.len() - 3..].iter().product()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ndarray::arr2;
+
+    const MOCK_DATA: [[u32; 10]; 5] = [
+        [2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
+        [3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
+        [9, 8, 5, 6, 7, 8, 9, 8, 9, 2],
+        [8, 7, 6, 7, 8, 9, 6, 7, 8, 9],
+        [9, 8, 9, 9, 9, 6, 5, 6, 7, 8],
+    ];
+
+    #[test]
+    fn test_answer1_mock_data() {
+        assert_eq!(answer1(&arr2(&MOCK_DATA)), 15)
+    }
+
+    #[test]
+    fn test_answer2_mock_data() {
+        assert_eq!(answer2(&arr2(&MOCK_DATA)), 1134)
+    }
+    #[test]
+    fn test_answer1() {
+        assert_eq!(answer1(&load(DATA)), 554)
+    }
+
+    #[test]
+    fn test_answer2() {
+        assert_eq!(answer2(&load(DATA)), 1017792)
+    }
+}
--- src/loaders.rs
+++ src/loaders.rs
@@ -1,6 +1,7 @@
+use ndarray::Array2;
 use std::{
     fmt::Debug,
-    fs::File,
+    fs::{read_to_string, File},
     io::{prelude::*, BufReader},
     path::Path,
     str::FromStr,
@@ -16,9 +17,8 @@ where
     T::Err: Debug,
     T: FromStr,
 {
-    buf_open(filename).lines().map(|l| {
-        l.expect("Could not parse line")
-            .parse::<T>()
+    file_to_lines(filename).map(|l| {
+        l.parse::<T>() // should this be TryFrom?
             .expect("Failed to parse")
     })
 }
@@ -29,6 +29,16 @@ where
     T: FromStr,
 {
     file_to(filename).collect()
+}
+
+pub fn file_to_array2(filename: impl AsRef<Path>) -> Array2<u32> {
+    let data = read_to_string(filename).expect("Failed to open file");
+    let width = data
+        .chars()
+        .position(|c| c == '\n')
+        .unwrap_or_else(|| data.len() - 1);
+    let data: Vec<u32> = data.chars().filter_map(|c| c.to_digit(10)).collect();
+    Array2::from_shape_vec((data.len() / width, width), data).expect("Could not build array")
 }
 
 pub fn delimited_file_to<T>(filename: impl AsRef<Path>, delim_byte: u8) -> impl Iterator<Item = T>
@@ -45,8 +55,26 @@ where
     })
 }
 
+// pub fn file_chars_as_digits(filename: impl AsRef<Path>) -> impl Iterator<Item = u32> {
+//     buf_open(filename).bytes().map(|byte| {
+//         (byte.expect("Unexpected IO interruption") as char)
+//             .to_digit(10)
+//             .expect("Failed to parse")
+//     })
+// }
+
 pub fn file_to_lines(filename: impl AsRef<Path>) -> impl Iterator<Item = String> {
     buf_open(filename)
         .lines()
         .map(|line| line.expect("Couldn't read line"))
 }
+
+// pub fn file_to_paragraphs(filename: impl AsRef<Path>) -> impl Iterator<Item = Vec<String>> {
+//     // I wish I could figure out how to make this an iterator of iterators, but I'm having trouble
+//     // with the lifetimes.
+//     // Perhaps the issue lies in https://github.com/rust-lang/rust/issues/61756 ?
+//     file_to_lines(filename).peekable().batching(|lines| {
+//         lines.peek()?; // Don't keep iterating if the iterator is empty
+//         Some(lines.take_while(|line| !line.is_empty()).collect())
+//     })
+// }
